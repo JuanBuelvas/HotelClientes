9d1f86848ec5c102ce16902ddac221a562281ad8
201 <201@academicos.cesde.edu.co>
2022-09-19 20:55:32 -0500

    Se crean tarjetas

--- src/Actividades/Actividades.js
+++ src/Actividades/Actividades.js
@@ -4,7 +4,27 @@ export function Actividades(){
             <div className="container">
                 <div className="row">
                     <div className="col-12 col-md-4">
-
+                        {/* <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                            <div className="carousel-inner">
+                                <div className="carousel-item active">
+                                    <img src="https://firebasestorage.googleapis.com/v0/b/logincomfamajb.appspot.com/o/012-Suite-Amaral-terraza-270x270.jpg?alt=media&token=124f4e8e-a9f2-4c18-9d80-11c30d1acb29" ></img>
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="https://firebasestorage.googleapis.com/v0/b/logincomfamajb.appspot.com/o/014-Suite-Amaral-terraza-jacuzzi-270x270.jpg?alt=media&token=96869225-5fde-4ff9-92aa-83591c97c356" ></img>
+                                </div>
+                                <div className="carousel-item">
+                                    <img src="https://firebasestorage.googleapis.com/v0/b/logincomfamajb.appspot.com/o/017-Suite-Amaral-jacuzzi-di%E2%95%A0%C3%BCa-270x270.jpg?alt=media&token=5e192c8b-36f5-4df6-bce5-5d28d67cc7c6" ></img>
+                                </div>
+                            </div>
+                            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Previous</span>
+                            </button>
+                            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Next</span>
+                            </button>
+                        </div> */}
                     </div>
                     <div className="col-12 col-md-8">
                         {/* https://www.sofitellegendsantaclara.com/es/ */}
@@ -20,30 +40,29 @@ export function Actividades(){
                             Sus murallas, que en tiempos remotos sirvieron de escudo, armonizan con balcones coloniales y edificaciones republicanas evocando el esplendor de otras épocas. En 1995, luego de un gran trabajo, el Sofitel Legend Santa Clara Cartagena abrió sus puertas como un hotel de lujo lleno de historia, magia e impactantes anécdotas…”
                         </p>
 
-                        {/* <div class="card" style="width: 18rem;">
-                            <img src="..." class="card-img-top" alt="..."/>
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <div className="row mt-5">
+                            <div className="col-12 col-md-4">
+                                <img src="https://firebasestorage.googleapis.com/v0/b/logincomfamajb.appspot.com/o/012-Suite-Amaral-terraza-270x270.jpg?alt=media&token=124f4e8e-a9f2-4c18-9d80-11c30d1acb29" className="img-fluid w-100 h-100"></img>
+                            </div>
+                            <div className="col-12 col-md-4">
+                                <img src="https://firebasestorage.googleapis.com/v0/b/logincomfamajb.appspot.com/o/014-Suite-Amaral-terraza-jacuzzi-270x270.jpg?alt=media&token=96869225-5fde-4ff9-92aa-83591c97c356" className="img-fluid w-100 h-100"></img>
+                            </div>
+                            <div className="col-12 col-md-4">
+                                <img src="https://firebasestorage.googleapis.com/v0/b/logincomfamajb.appspot.com/o/017-Suite-Amaral-jacuzzi-di%E2%95%A0%C3%BCa-270x270.jpg?alt=media&token=5e192c8b-36f5-4df6-bce5-5d28d67cc7c6" className="img-fluid w-100 h-100"></img>
                             </div>
                         </div>
-                        <div class="card" style="width: 18rem;">
-                            <img src="..." class="card-img-top" alt="..."/>
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+
+                        <div className="row mt-5">
+                            <div className="col-12 col-md-4">
+                                <img src="https://firebasestorage.googleapis.com/v0/b/logincomfamajb.appspot.com/o/Sofitel_legend_santaclara_elclaustro_gallery03-270x270.jpg?alt=media&token=443d7293-61c5-4508-b552-478a4dc4dd77" className="img-fluid h-100 w-100"></img>
+                            </div>
+                            <div className="col-12 col-md-4">
+                                <img src="https://firebasestorage.googleapis.com/v0/b/logincomfamajb.appspot.com/o/Sofitel_legend_santaclara_elclaustro_gallery04-270x270.jpg?alt=media&token=68601b8a-32aa-4312-81f9-28451761a812" className="img-fluid h-100 w-100"></img>
+                            </div>
+                            <div className="col-12 col-md-4">
+                                <img src="https://firebasestorage.googleapis.com/v0/b/logincomfamajb.appspot.com/o/Sofitel_legend_santaclara_poolbar_gallery01-270x270.jpg?alt=media&token=1c8c9d1f-e1ef-416a-97b4-104f778571d0" className="img-fluid h-100 w-100"></img>
                             </div>
                         </div>
-                        <div class="card" style="width: 18rem;">
-                            <img src="..." class="card-img-top" alt="..."/>
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div> */}
 
                     </div>
                 </div>
--- src/Habitaciones/Habitaciones.js
+++ src/Habitaciones/Habitaciones.js
@@ -28,16 +28,33 @@ export function Habitaciones(params) {
     } else {
         return(
             <div>
-                {
-                    habitaciones.datos.map(function (habitacion) {
-                        return(
-                            <div>
-                                <h1>{habitacion.nombre}</h1>
-                                <img src={habitacion.imagenes[0]}></img>
-                            </div>
-                        );
-                    })
-                }
+                <div className="container-fluid p-5 habitaciones">
+                    <div className="row row-cols-1 row-cols-md-2 g-5">
+                        {
+                            habitaciones.datos.map(function (habitacion) {
+                                return(
+                                    <>
+                                        {/* <div>
+                                            <h1>{habitacion.nombre}</h1>
+                                            <img src={habitacion.imagenes[0]}></img>
+                                        </div> */}
+
+                                        <div>
+                                            <div className="card h-100">
+                                                <img src={habitacion.imagenes[0]} className="card-img-top w-100 h-100" alt="..."></img>
+                                                    <div className="card-body">
+                                                    <h5 className="card-title">{habitacion.nombre}</h5>
+                                                    <p className="card-text">{habitacion.descripcion}</p>
+                                                    {/* <a href="/" className="btn btn-primary">Go somewhere</a> */}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </>
+                                );
+                            })
+                        }
+                    </div>
+                </div>
             </div>
         );
     }
